4f8958328caba4fb4b524d0bc0e7b05707b5641e
Anastasia <aad39350@gmail.com>
2019-10-19 12:34:34 +0300

    script to index

--- js/main.js
+++ js/main.js
@@ -1,41 +1,5 @@
 "use strict";
 
-const output = document.querySelector('.modal__value');
-const rangeSLider = document.querySelector('.adjust-bar.adjust-bar_theme_temp');
-const arrowLeftDevs = document.querySelector('.devices__paginator .paginator__arrow_left');
-const arrowRightDevs = document.querySelector('.devices__paginator .paginator__arrow_right');
-const panelCountDevs = document.querySelectorAll('.devices__panel').length;
-const devices = document.querySelector('.devices');
-const pagiantorDevs = document.querySelector('.devices__paginator');
-let currentPageDevs = 1;
-
-let curValue;
-let curRotate;
-let maxRotate = 0.42; // 150 градусов
-let minRotate = -0.42; // -150 градусов
-
-const MIN_VALUE = 26;
-const MAX_VALUE = 35;
-const INDICATOR_OFFSET = 265;
-
-let knobDragged;
-let prevAngleRad = null;
-let prevRotate = null;
-
-const arrowLeftScens = document.querySelector('.scenarios__paginator .paginator__arrow_left');
-const arrowRightScens = document.querySelector('.scenarios__paginator .paginator__arrow_right');
-const panelCountScens = document.querySelectorAll('.scenarios__panel').length;
-const pageCountScens = document.querySelectorAll('.scenarios__page').length;
-const scenarios = document.querySelector('.scenarios');
-const pagiantorScens = document.querySelector('.scenarios__paginator');
-let currentPage = 1;
-
-const selectButton = document.querySelector('.filter__select-button');
-const selectButtonText = document.querySelector('.filter__select-button .button__text');
-const selectOptions = document.querySelectorAll('.filter__select-item');
-const popup = document.querySelector('.filter__select-popup');
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
@@ -68,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 500)
 
 
-    document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function() {
-        document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
-    })
+    // document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function() {
+    //     document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
+    // })
 }, !1);
